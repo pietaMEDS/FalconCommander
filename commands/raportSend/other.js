7418d8a1b697ad309b4700a57eb0c937236f0522
pietaMEDS <maxibanp@gmail.com>
2024-11-04 18:05:53 +0500

    FIX:  Organize undefinded

--- commands/raportSend/other.js
+++ commands/raportSend/other.js
@@ -1,35 +1,12 @@
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, Colors, Embed, EmbedBuilder, ModalBuilder, REST, Routes, TextInputBuilder, TextInputStyle  } from 'discord.js';
 import { readFile } from 'fs/promises';
 import MemberSort from '../utility/MemberSort.js';
-export default async (interaction) => {
+export default async (interaction, client) => {
 
     let users = JSON.parse( await readFile("./data/users.json"))
     let soldiers = interaction.fields.fields.get("soldiers").value.split(",")
-    let results = interaction.fields.fields.get("result").value.split(",")
-    let activies = ''
-    let ResponseResults = ''
-
-
-    soldiers.forEach(soldier => {
-        if(soldier.trim().split(" ").length == 1){
-            users.users.forEach(member => {
-                if (soldier.toLowerCase().trim() == member.name.toLowerCase().trim()) {
-                    activies+="- "+member.guild+" "+member.rank.name+" "+member.id+" "+member.name+`
-`
-                }
-            })
-        }
-        else{
-            activies+="- "+soldier.trim()+`
-`
-        }
-     });
-
-     results.forEach(result => {
-        ResponseResults+= result.trim()+`
-`
-     });
-
+    let activies = await MemberSort(interaction, soldiers, users, interaction.member.nickname)
+    
     let date = new Date();
     let day;
     if (date.getDate() < 10) {
@@ -38,43 +15,43 @@ export default async (interaction) => {
     else{
         day = date.getDate()
     }
-    let month = date.getMonth()+1    
-    
-    try{
+    let month = date.getMonth()+1
+
+    if (activies == false) {
+        interaction.reply({content:"Напоминаю! рапорта пишутся старшим в подразделении на момент события!", ephemeral:true})
+        return;
+    }
+
+    try {
         let replyEmbed = new EmbedBuilder()
         .setColor("#ffffff")
-        .setTitle("*Рапорт о проведении обучения*")
+        .setTitle("*Рапорт о проведении лекции*")
         .setAuthor({name: "Организатор: "+interaction.member.nickname, iconURL: "https://cdn.discordapp.com/avatars/"+interaction.user.id+"/"+interaction.user.avatar })
         .addFields(
-            { name: "Специализация: ", value: interaction.fields.fields.get("spec").value },
+            { name: `Лекция на тему "` + interaction.fields.fields.get("lecture").value + `"`, value: " " },
             { name: '\u200B', value: ' ' },
-            { name: "Участники", value:activies, inline: true  },
-            { name: ' ', value: ' ', inline:true },
-            { name: "Результаты", value: ResponseResults, inline: true  },
-            { name:"\u200B", value:"Дата: "+ day +"."+month } 
+            { name:"Участники", value:activies, inline: true  },
+            { name:"\u200B", value:"Дата: "+day+"."+month } 
         )
 
         let confirm = new ButtonBuilder()
-            .setCustomId('confirm:Officer')
-			.setLabel('Одобрить рапорт')
-			.setStyle(ButtonStyle.Success)
-            .setEmoji('1233846475805425835');
+        .setCustomId('confirm:Officer')
+        .setLabel('Одобрить рапорт')
+        .setStyle(ButtonStyle.Success)
+        .setEmoji('1233846475805425835');
 
-        let cancel = new ButtonBuilder()
-            .setCustomId('cancel:Officer')
-			.setLabel('Отклонить рапорт')
-			.setStyle(ButtonStyle.Danger)
-            .setEmoji('1233846477470830654');
+    let cancel = new ButtonBuilder()
+        .setCustomId('cancel:Officer')
+        .setLabel('Отклонить рапорт')
+        .setStyle(ButtonStyle.Danger)
+        .setEmoji('1233846477470830654');
 
-        let ButtonComponent = new ActionRowBuilder().addComponents(confirm, cancel)
+    let ButtonComponent = new ActionRowBuilder().addComponents(confirm, cancel)
 
         interaction.reply({embeds: [replyEmbed], components: [ButtonComponent] })
+    } catch (error) {
+        console.log(error);
     }
-    catch (err){
-        console.log(err);
-    }
-
-    
 }
 
 
